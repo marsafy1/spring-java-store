ef6018904680f4f61c6ae03fa0143badcc327513
marsafyyy <mokhallid72@gmail.com>
2024-04-21 18:26:07 +0200

    Created the model in order service

---
+++ order-service-maven/src/main/java/model/Order.java
@@ -0,0 +1,15 @@
+package model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderName;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLineItems> orderLineItemsList;
+
+}
---
+++ order-service-maven/src/main/java/model/OrderLineItems.java
@@ -0,0 +1,23 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLineItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+}
